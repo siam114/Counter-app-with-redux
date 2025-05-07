08c5de22215ef0810cc44acfbd4ed3ae55174d2d
siam114 <smsiamislam598@gmail.com>
2025-05-07 17:11:18 +0600

    command redux plan'

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,7 +6,7 @@ const Counter = () => {
     const handleIncrement = () => {
         setCount(count + 1)
     }
-    
+
   return (
     <div>
       <h2>Counter App</h2>
@@ -18,3 +18,13 @@ const Counter = () => {
 }
 
 export default Counter
+
+
+
+
+//state - count : 0 
+//action - increment, decrement, reset
+//reducers- increment -> count => count + 1
+//           decrement -> count => count - 1
+//           reset -> count => 0
+//store
