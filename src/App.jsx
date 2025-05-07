47fc2ce7447c02e7eafc34f06728e8bf80e9dd98
siam114 <smsiamislam598@gmail.com>
2025-05-07 17:55:07 +0600

    counter with redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Counter from './components/Counter'
+import CounterRedux from './components/CounterRedux'
 
 function App() {
 
   return (
     <div className="App">
       <h1>Welcome to Redux</h1>
-      <Counter/>
+      {/* <Counter/> */}
+      <CounterRedux/>
     </div>
   )
 }
---
+++ src/components/CounterRedux.jsx
@@ -0,0 +1,13 @@
+const CounterRedux = () => {
+
+    return (
+        <div>
+      <h2>Counter App</h2>
+
+      <h3>Count : 0</h3>
+      <button >Increment</button>
+    </div>
+    );
+};
+
+export default CounterRedux;
