fa06122445a8e07f1be178a85a132cf9bb6eaf9a
siam114 <smsiamislam598@gmail.com>
2025-05-07 17:06:05 +0600

    set up and create counter without redux

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Welcome to Redux</h1>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const handleIncrement = () => {
+        setCount(count + 1)
+    }
+    
+  return (
+    <div>
+      <h2>Counter App</h2>
+
+      <h3>Count : {count}</h3>
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  )
+}
+
+export default Counter
