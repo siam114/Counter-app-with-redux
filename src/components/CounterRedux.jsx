67c659a067a4c0b8c65d6360bc8b7dc8796194c9
siam114 <smsiamislam598@gmail.com>
2025-05-07 18:13:02 +0600

    counter with redux complete

--- src/components/CounterRedux.jsx
+++ src/components/CounterRedux.jsx
@@ -1,11 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrementCounter, incrementCounter, resetCounter } from "../services/actions/counterAction";
+
 const CounterRedux = () => {
+    const count = useSelector(state => state.count)
+    const dispatch = useDispatch();
+
+    const handleIncrement = () =>{
+        dispatch(incrementCounter())
+    }
+
+    const handleDecrement = () =>{
+        dispatch(decrementCounter())
+    }
+
+    const handleReset = () =>{
+        dispatch(resetCounter())
+    }
 
     return (
         <div>
       <h2>Counter App</h2>
 
-      <h3>Count : 0</h3>
-      <button >Increment</button>
+      <h3>Count : {count}</h3>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
     );
 };
